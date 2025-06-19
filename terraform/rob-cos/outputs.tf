978c4fd4a7341512777a9b3b182b0b0a2c537919
artivis <jeremie.deray@canonical.com>
2025-06-19 09:52:09 +0200

    add blackbox to outputs
    Signed-off-by: artivis <jeremie.deray@canonical.com>

--- terraform/rob-cos/outputs.tf
+++ terraform/rob-cos/outputs.tf
@@ -1,11 +1,17 @@
 output "app_names" {
   value = merge(
     {
-      cos_registratio_server = module.cos_registration_server.app_name,
-      foxglove_studio        = module.foxglove_studio.app_name,
+      blackbox_exporter      = module.blackbox_exporter.app_name,
+      cos_registratio_server = module.robcos_overlay.cos_registration_server.app_name,
+      foxglove_studio        = module.robcos_overlay.foxglove_studio.app_name,
     }
   )
   description = "The names of the deployed applications"
+}
+
+output "blackbox_exporter" {
+  description = "Outputs from the Blackbox-exporter module"
+  value       = module.blackbox_exporter
 }
 
 output "cos_lite" {
