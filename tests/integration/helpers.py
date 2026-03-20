6048be18c0256420eb3c0b5e428e9ffdbd39c48f
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2026-03-20 16:13:02 +0100

    test(integration): use snap watch to wait for all snaps to be installed

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -61,11 +61,7 @@ def ros_domain_cloud_init_config(ros_domain_id: int = 0) -> str:
         "    append: true\n"
         "runcmd:\n"
         "  - snap wait system seed.loaded\n"
-        "  - |\n"
-        "    while snap changes | grep -q 'Doing'; do\n"
-        '      echo "Waiting for all initial seeded snaps to finish installing..."\n'
-        "      sleep 5\n"
-        "    done\n"
+        "  - snap watch --last=install?\n"
         "  - snap refresh\n"
     )
 
